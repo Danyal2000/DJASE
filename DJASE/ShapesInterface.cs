c2063a418a84736c108f8ca86c050f9d4c859b93
Danyal <d.javed2585@student.leedsbeckett.ac.uk>
2022-11-20 21:10:12 +0000

    Setup Shape interface with two non-defined methods and appropriate parameters. These methods wiare Draw and Fill. These have been inherited by a shape class and passed onto child classes to be defined. The child classes include Triangle etc. The structure of these classes has been setup, but the inherited methods are yet to be defined.

---
+++ DJASE/ShapesInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJASE
+{
+    interface IShapes
+    {
+
+
+        void Draw(Graphics g, Pen pen, int xPos, int yPos);
+        void Fill(Graphics g, Pen pen, Brush brush, int xPos, int yPos);
+    }
+}
