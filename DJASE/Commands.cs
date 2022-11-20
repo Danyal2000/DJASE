2582cf8feae95c9299a6fef83818d9a0381256b0
Danyal <d.javed2585@student.leedsbeckett.ac.uk>
2022-11-20 22:35:10 +0000

    commit

--- DJASE/Commands.cs
+++ DJASE/Commands.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace DJASE
 {
-    internal class Commands
+    public class Commands
     {
+        public string[] inputarr(string input)
+        {
+            string str = input.ToLower().Trim();
+
+            string[] ar = str.Split('\n');
+            return ar;
+        }
     }
 }
+
--- DJASE/Rectangle.cs
+++ DJASE/Rectangle.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace DJASE
 {
-    internal class Rectangle
+    class Rectangle : Shape
     {
+        private int height;
+        private int width;
+
+       
+
+         public void SetHeightWidth(int h,int w) 
+        {
+            height = h;
+            width = w;
+        }
+
+      
+        public override void Draw(Graphics g, Pen pen, int xPos, int yPos)
+        {
+            g.DrawRectangle(pen, xPos, yPos, width, height);
+        }
+
+        public override void Fill(Graphics g,Pen pen, Brush brush, int xPos, int yPos)
+        {
+           g.FillRectangle(brush, xPos, yPos, width, height);
+        }
     }
 }
